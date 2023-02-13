a14d46d08dee603f14399cdd991eb38ca9c71908
Caroline Viana <carolviana-21@hotmail.com>
2023-02-13 15:38:13 -0300

    Inserindo mais exercícios

--- src/aula_04/Atividade1_matriz.java
+++ src/aula_04/Atividade1_matriz.java
@@ -7,20 +7,44 @@ public class Atividade1_matriz {
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
 
-		int matriz[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-		int soma;
+		int matriz[][] = new int[3][3];
+		int somaPrincipal = 0;
+		int somaSecundaria = 0;
+		
+		for (int indiceI = 0; indiceI < matriz.length; indiceI++) {
+			for (int indiceJ = 0; indiceJ < matriz.length; indiceJ++) {
+				System.out.println("Digite um valor real: ");
+				matriz[indiceI][indiceJ] = leia.nextInt();
+			}
+		}
+		
 
 		for (int indiceI = 0; indiceI < matriz.length; indiceI++) {
 			for (int indiceJ = 0; indiceJ < matriz.length; indiceJ++) {
 				if (indiceI == indiceJ) {
 					System.out.println(matriz[indiceI][indiceJ]);
-
 				}
-
 			}
-
 		}
-
+		
+		for (int indiceI = 0; indiceI < matriz.length; indiceI++) {
+			for (int indiceJ = 0; indiceJ < matriz.length; indiceJ++) {
+				if ((indiceI + indiceJ) == (matriz.length -1)) {
+					System.out.println(matriz[indiceI][indiceJ]);
+				}
+			}
+		}
+		
+		for (int diagonalPrincipal = 0; diagonalPrincipal < matriz.length; diagonalPrincipal++) {
+			somaPrincipal += matriz[diagonalPrincipal][diagonalPrincipal];
+			somaSecundaria += matriz[matriz.length - 1 - diagonalPrincipal][diagonalPrincipal];
+		}
+		
+		System.out.println("Soma dos Elementos da Diagonal Principal: " + somaPrincipal);
+		System.out.println("Soma dos Elementos da Diagonal Principal: " + somaSecundaria);
+		
+		leia.close();
+		
 	}
 
 }
